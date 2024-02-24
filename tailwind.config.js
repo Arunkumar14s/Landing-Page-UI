3fa1ddbd16fa2483a928544ab12bfa79bf03c5b5
Arunkumar14s <arunkumarsekar5@gmail.com>
2024-02-24 20:07:55 +0530

    Vite New File

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+
+  //   colors: {
+  //     'Very-Pale-Cyan': 'hsl(193, 100%, 96%)', #ebfbff
+  //     'Very-Dark-Cyan': 'hsl(192, 100%, 9%)',  #00252e
+  //     'Grayish-Blue': 'hsl(208, 11%, 55%)',  #808d99
+
+  //     'orangee': 'hsl(35, 77%, 62%)', #e9ab53
+  //     'redd': 'hsl(5, 85%, 63%)' #f15e50
+//  pink : #ff52bf
+  // },
+
+    extend: {},
+  },
+  plugins: [],
+}
+
